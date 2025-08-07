96428da477eb1da7d411c3010a354a14ed39f037
dannyboi07 <danielchettiar07@gmail.com>
2025-08-07 21:51:43 +0530

    Directly quits the app for MacOS

--- src/main/index.js
+++ src/main/index.js
@@ -251,9 +251,7 @@ function createMainWindow() {
 }
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    app.quit();
 });
 
 app.on("activate", () => {
